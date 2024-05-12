7c92c7b8509121003779f52c4d4edb2f0c62ecfd
“Mariana <workmaryana@gmail.com>
2024-05-13 00:17:07 +0300

    added the API tests and custom commands

--- cypress/e2e/create-car.cy.js
+++ cypress/e2e/create-car.cy.js
@@ -4,39 +4,35 @@ import {fuelExpensesStep} from "../../steps/fuel-expenses-step";
 import GaragePage from "../../pages/GaragePage";
 import {baseButtons} from "../../pages/BaseButtons";
 
-const car={
+const today = new Date();
+const tomorrow = new Date(today);
+
+const carData={
     brand: 'Ford',
     model: 'Fusion',
-    mileage: '10'
+    mileage: 10
 }
 
-const fuelExpense={
-    vehicle: car.brand + " " + car.model,
-    date: parseDate(),
-    mileage: car.mileage,
-    liters: '10',
-    cost: '500'
-}
+const expenseData = {
+    "reportedAt": "2024-05-12",
+    "mileage": 20,
+    "liters": 11,
+    "totalCost": 11,
+    "forceMileage": false
+};
 const email='mariana@mariana.com';
 const password='QTdvXj4!FSsuH2';
 
 function parseDate(){
     let currentDate = new Date();
     let day = currentDate.getDate();
-    let month = currentDate.getMonth() + 1;
+    let month = currentDate.getMonth();
     let year = currentDate.getFullYear();
     return day + '.' + month + '.' + year;
 }
 
-function parseDateFromDataPicker(targetDate){
-    const formattedDate=targetDate.toLocaleString(`en-US`,{
-        weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
 
-    cy.get(`.modal-body`).within(()=>{
-        cy.get('.ngb-dp-day[aria-label="' + formattedDate + '"]')
-            .should(`be.visible`).click()
-    });
-}
+let carId;
 
 describe('Test Suite', () => {
 
@@ -46,35 +42,53 @@ describe('Test Suite', () => {
         generalStep.verifyLoginButtonIsVisible();
     });
 
-    it('Check the adding new car',()=>{
-        garageStep.addNewCar(car)
-        garageStep.checkCarIsAdded();
+    it('Check the adding new car', () => {
+
+        cy.intercept('POST', 'api/cars').as('addCar');
+        garageStep.addNewCar(carData);
+
+        cy.wait('@addCar').then((response) => {
+            const res = response.response;
+            cy.writeFile('cypress/fixtures/carsResponseData.json', res);
+            cy.wrap(res.body.data.id).then(id => {
+                carId = id;
+            });
+        });
+
+        garageStep.checkNewCarAddedViaAPI(carData, carId);
     });
 
-    it('Check the car removing',()=>{
-        garageStep.removeCar();
-        garageStep.checkCarIsDeleted();
-
+    it('Check the adding the fuel expense via API', () => {
+        cy.loginViaAPI(email, password);
+        cy.addAnExpense(expenseData,carId);
     });
 
-    it(`Check the date is selected from dataPicker`,()=>{
-        const today=new Date();
-        const tomorrow=new Date(today);
-        tomorrow.setDate(tomorrow.getDate()+1);
-        garageStep.selectDateFromDataPicker();
-        parseDateFromDataPicker(tomorrow);
-        baseButtons.baseModalCloseButton().click();
-    });
+    it('Check the expense is added from API',()=>{
+        cy.visit(`/`);
+        generalStep.login(email, password);
+        generalStep.verifyLoginButtonIsVisible();
+        cy.visit('/panel/expenses');
+        fuelExpensesStep.findTheAddedCarFromAPI(carData,expenseData);
 
-    it('Check the adding the fuel expense', () => {
-        garageStep.addAnExpense(fuelExpense);
-        garageStep.checkFuelExpenseIsAdded();
-    });
+    })
 
-    it('Check the expense removing',()=>{
-        fuelExpensesStep.deleteExpense();
-        fuelExpensesStep.checkExpenseIsDeleted();
-    });
+        it.skip('Check the car removing', () => {
+            garageStep.removeCar();
+            garageStep.checkCarIsDeleted();
+
+        });
+
+        it.skip(`Check the date is selected from dataPicker`, () => {
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            garageStep.selectDateFromDataPicker();
+            cy.parseDateFromDataPicker(tomorrow);
+            baseButtons.baseModalCloseButton().click();
+        });
+
+        it.skip('Check the expense removing', () => {
+            fuelExpensesStep.deleteExpense();
+            fuelExpensesStep.checkExpenseIsDeleted();
+        });
+});
 
 
-})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,79 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import HomePage from "../../pages/HomePage";
+import {baseButtons} from "../../pages/BaseButtons";
+Cypress.Commands.add('loginViaAPI',(email,password, remember=false)=>{
+    cy.request({
+        method: 'POST',
+        url: '/api/auth/signin',
+        body:
+            {
+                "email": email,
+                "password": password,
+                "remember": remember
+            }
+    }).then(response=>{
+        expect(response.status).to.eq(200);
+        cy.log(response.body);
+        return cy.wrap(response.body.data.userId).as('userId');
+    });
+});
+Cypress.Commands.add('loginViaUI',(email, password) => {
+    HomePage.signInButton.click();
+    HomePage.signInEmail.type(email);
+    HomePage.signInPassword.type(password);
+    baseButtons.baseButton('Login').click();
+});
+Cypress.Commands.add('saveLocalStorage', ()=>{
+    cy.wrap(localStorage).as('localStorage');
+    cy.log(localStorage);
+})
+Cypress.Commands.add('restoreLocalStorage', ()=>{
+    cy.get('@localStorage').then(data => {
+        localStorage.clear();
+        for (let key in data){
+            cy.log(data[key]);
+            localStorage.setItem(key,data[key]);
+        }
+    })
+})
+Cypress.Commands.add('addAnExpense',(fuelExpense,carId)=> {
+    cy.request({
+        method: 'POST',
+        url: '/api/expenses',
+        body:
+            {
+                ...fuelExpense,
+                carId: carId
+            }
+    }).then((response) => {
+        expect(response.status).to.eq(200);
+        return response.body.data;
+        });
+
+    });
+
+Cypress.Commands.add('parseDateFromDataPicker',(targetDate)=>{
+    const formattedDate=targetDate.toLocaleString(`en-US`,{
+        weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
+
+    cy.get(`.modal-body`).within(()=>{
+        cy.get('.ngb-dp-day[aria-label="' + formattedDate + '"]')
+            .should(`be.visible`).click()
+    });
+});
+
+Cypress.Commands.add('generateRandomEmail',()=>{
+    const domain = 'mariana.com';
+    const length = 10;
+    let username = '';
+    let intro = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        let index = Math.floor(Math.random() * intro.length);
+        username += intro.charAt(index);
+    }
+
+    return username + '@' + domain;
+});
+
+
+
